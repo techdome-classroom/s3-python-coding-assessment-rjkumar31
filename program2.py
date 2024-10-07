de76159f117a6d60e5fa40d8214e4cb5aa0fbad4
rjkumar31 <rahulnuq787025@gmail.com>
2024-10-07 15:48:39 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,4 @@
 class Solution(object):
-    class Solution(object):
     def romanToInt(self, s):
         roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
