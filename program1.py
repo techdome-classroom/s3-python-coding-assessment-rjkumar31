c3ab9dc8150c2457a7e1f3d2826eb41c2be6bb7c
rjkumar31 <rahulnuq787025@gmail.com>
2024-10-07 15:24:52 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,42 +1,13 @@
-class Solution(object):
-    def isValid(self, s):
-       
-   
-        stack = []
+ bracket_map = {')': '(', '}': '{', ']': '['}
+    # Stack to keep track of open brackets
+    stack = []
 
-        # Map of closing brackets to their corresponding opening brackets
-        bracket_map = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
-
-    
-        for char in s:
-            
-            if char in bracket_map:
-            
-                top_element = stack.pop() if stack else '#'
-                
-                
-                if bracket_map[char] != top_element:
-                    return False
+    for char in s:
+        if char in bracket_map.values():  # Check if it's an open bracket
+            stack.append(char)
+        elif char in bracket_map.keys():  # Check if it's a closing bracket
+            if stack and stack[-1] == bracket_map[char]:  # Check if the last open matches
+                stack.pop()  # Remove the last open bracket
             else:
-            
-                stack.append(char)
-
-        
-        return len(stack) == 0
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+                return False  # Mismatched or unbalanced
+    return not stack  # Valid if stack is empty at the end
