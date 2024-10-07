c3253cee71f5d8abe2fb5c6b01cf8e8877dea636
rjkumar31 <rahulnuq787025@gmail.com>
2024-10-07 15:39:28 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,32 +2,6 @@ class Solution(object):
     def isValid(self, s):
        
    
-        # stack = []
-
-        # # Map of closing brackets to their corresponding opening brackets
-        # bracket_map = {
-        #     ')': '(',
-        #     '}': '{',
-        #     ']': '['
-        # }
-
-    
-        # for char in s:
-            
-        #     if char in bracket_map:
-            
-        #         top_element = stack.pop() if stack else '#'
-                
-                
-        #         if bracket_map[char] != top_element:
-        #             return False
-        #     else:
-            
-        #         stack.append(char)
-
-        
-        # return len(stack) == 0
-
        
         stack = []
         opening = '({['
