294a417fb1b6416b3d35767bce1fb57e20f2e48c
rjkumar31 <rahulnuq787025@gmail.com>
2024-10-07 15:10:37 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,14 +1,19 @@
 class Solution(object):
-   
-    def isValid(self,s):
-   
-        stack = []
-
-        # Map of closing brackets to their corresponding opening brackets
-        bracket_map = {
-            ')': '(',
-            '}': '{',
-            ']': '['
+   class Solution(object):
+    def romanToInt(self, s):
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
+        
+        for char in s[::-1]:
+            value = roman_map[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        
+        return total
         
     
 
