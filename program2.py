bc9041103a4278e46a16e8b587ee4eb068c0fda5
rjkumar31 <rahulnuq787025@gmail.com>
2024-10-07 15:10:24 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -15,17 +15,6 @@ class Solution(object):
         for char in s:
             
             if char in bracket_map:
-            
-                top_element = stack.pop() if stack else '#'
-                
-                
-                if bracket_map[char] != top_element:
-                    return False
-            else:
-            
-                stack.append(char)
-
-        
-        return len(stack) == 0
+    
 
 
