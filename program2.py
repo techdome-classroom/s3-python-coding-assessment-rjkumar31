0a3b719a19f1ae1e77fdea672994dbbae5880b0d
rjkumar31 <rahulnuq787025@gmail.com>
2024-10-07 15:10:05 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -14,18 +14,18 @@ class Solution(object):
     
         for char in s:
             
-            if char in bracket_map:
-            
-                top_element = stack.pop() if stack else '#'
-                
-                
-                if bracket_map[char] != top_element:
-                    return False
-            else:
-            
-                stack.append(char)
-
+           class Solution(object):
+    def romanToInt(self, s):
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
         
-        return len(stack) == 0
-
-
+        for char in s[::-1]:
+            value = roman_map[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        
+        return total
