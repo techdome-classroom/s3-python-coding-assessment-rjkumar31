981d7daca42209671af175e1fb573b19c60b08e7
rjkumar31 <rahulnuq787025@gmail.com>
2024-10-07 15:25:12 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -28,6 +28,20 @@ class Solution(object):
         
         # return len(stack) == 0
 
+         bracket_map = {')': '(', '}': '{', ']': '['}
+    # Stack to keep track of open brackets
+    stack = []
+
+    for char in s:
+        if char in bracket_map.values():  # Check if it's an open bracket
+            stack.append(char)
+        elif char in bracket_map.keys():  # Check if it's a closing bracket
+            if stack and stack[-1] == bracket_map[char]:  # Check if the last open matches
+                stack.pop()  # Remove the last open bracket
+            else:
+                return False  # Mismatched or unbalanced
+    return not stack  # Valid if stack is empty at the end
+
 
 
 
